7297559c1e7813b94d0036c009e79ac4fef40c05
whatcodehaveyougit <siggywatt@yahoo.com>
2022-10-05 20:32:53 +0200

    drawer now looking acceptable

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,20 +1,17 @@
 import React, { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
-import logo1 from '../../logo.svg'
+import pickbeamLogo from '../../logo.svg'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 import './header.css'
 import { Link } from '@reach/router'
 
-//drawer elements used
 import Drawer from "@mui/material/Drawer";
-// import CloseIcon from "@mui/icons-material/Close";
 import Divider from "@mui/material/Divider";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
 import Button from "@mui/material/Button";
 import { Typography } from "@mui/material";
 
@@ -38,27 +35,20 @@ export default function Header() {
         <Toolbar className="header-container">
         <Box
             component="img"
-            sx={{
-            // width: 350,
-            // maxHeight: { xs: 233, md: 167 },
-            // maxWidth: { xs: 350, md: 250 },
-            }}
             alt="Pickbeam-logo."
-            src={logo1}
+            src={pickbeamLogo}
         />
           <Button 
-          // className="hamburger-btn"
-          color="inherit"
-
-          onClick={toggleDrawer(true)}
-          aria-label="open drawer"
-          sx={{
-              display: {
-                sm: "block",
-                md: "none"
-              }
-            }}
-          >
+            color="inherit"
+            onClick={toggleDrawer(true)}
+            aria-label="open drawer"
+            sx={{
+                display: {
+                  sm: "block",
+                  md: "none"
+                }
+              }}
+            >
             <MenuIcon />
           </Button>
 
@@ -102,51 +92,62 @@ export default function Header() {
             Nous Contacter
           </Button>
 
-          {/* </IconButton> */}
-
           {/* The outside of the drawer */}
           <Drawer
-            //from which side the drawer slides in
             anchor="left"
-            //if open is true --> drawer is shown
             open={open}
-            //function that is called when the drawer should close
             onClose={toggleDrawer(false)}
-            //function that is called when the drawer should open
             // open={toggleDrawer(true)}
           >
-            {/* The inside of the drawer */}
             <Box
-              sx={{
-                p: 2,
-                height: 1,
-              }}
+             sx={{
+              backgroundColor: 'primary.main',
+            }}
             >
-              {/* when clicking the icon it calls the function toggleDrawer and closes the drawer by setting the variable open to false */}
-              <IconButton sx={{ mb: 2 }}>
-                {/* <CloseIcon onClick={toggleDrawer(false)} /> */}
-              </IconButton>
-
-              <Divider sx={{ mb: 2 }} />
-
-              <Box sx={{ mb: 2 }}>
-                <ListItemButton>
-                  <ListItemText primary="Services" />
-                </ListItemButton>
-
-                <ListItemButton>
-                  <ListItemText primary="Votre besoin" />
-                </ListItemButton>
-
-                <ListItemButton>
-                  <ListItemText primary="Ressources" />
-                </ListItemButton>
-                <Button className="btn-default" variant="contained">
-                  Nous contacter
-                </Button>
+              <Box className="drawer-logo-container">
+                <Box
+                    component="img"
+                    alt="Pickbeam-logo."
+                    src={pickbeamLogo}
+                    mb={1}
+                    p={3}
+                />
               </Box>
+             
+                    
+              <Divider
+                variant="fullWidth"
+                sx={{
+                backgroundColor: 'primary.contrastText',
+                borderBottomWidth: 5
+              }} />
 
+              <Box mb={2} p={3}>
+                <Box mb={3} mt={4}>
+                  <Typography mb={3} className="drawer-item" color="primary.contrastText">
+                    Services
+                    <ExpandMoreIcon />
+                  </Typography>
+                  <Typography  mb={3} className="drawer-item" color="primary.contrastText">
+                    Votre besoin
+                    <ExpandMoreIcon />
+                  </Typography>
+                  <Typography  mb={3} className="drawer-item" color="primary.contrastText">
+                    Ressources
+                    <ExpandMoreIcon />
+                  </Typography>
+                </Box>
+                <Box p={3}>
+                  <Button 
+                    color="secondary"
+                    variant="contained"
+                    >
+                    Nous contacter
+                  </Button>
+                </Box>
+              </Box>
             </Box>
+
           </Drawer>
         </Toolbar>
       </Container>
