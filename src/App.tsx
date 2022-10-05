51f86f2c77037a22293fafd5d89eecd76ff9e45e
whatcodehaveyougit <siggywatt@yahoo.com>
2022-10-05 21:59:44 +0200

    renamed componants so it all works now

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Root, Routes, addPrefetchExcludes } from 'react-static'
-import Header from './components/Header/header'
+import { Root, addPrefetchExcludes } from 'react-static'
+import Navigation from './components/Navigation/Navigation'
 import './app.css'
 
 // Any routes that start with 'dynamic' will be treated as non-static routes
@@ -9,7 +9,7 @@ addPrefetchExcludes(['dynamic'])
 function App() {
   return (
     <Root>
-      <Header />
+      <Navigation />
       <div>
         Helllo
       </div>
